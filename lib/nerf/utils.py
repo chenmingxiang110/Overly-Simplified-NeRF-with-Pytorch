49ac3a28009ab537510d32c8483c5d9cb3b10c58
Ming <chenmingxiang110@gmail.com>
2023-03-04 12:15:08 +0800

    Add files via upload

--- lib/nerf/utils.py
+++ lib/nerf/utils.py
@@ -34,3 +34,44 @@ def pose_spherical(theta, phi, radius):
     c2w = np.array([[-1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]]) @ c2w
     return c2w
 
+def get_rays(H, W, focal, c2w):
+    h, w = np.meshgrid(np.arange(W), np.arange(H), indexing="ij")
+    directions = np.stack([(w-W*.5)/focal, -(h-H*.5)/focal, -np.ones(w.shape)], -1)
+    rays_d = np.sum(directions[..., None, :] * c2w[:3, :3], -1)
+    rays_o = np.broadcast_to(c2w[:3, -1], rays_d, shape)
+    return rays_o, rays_d
+
+def compute_accumulated_transmittance(alphas):
+    # formula 3 in the original paper
+    acc = torch.cumprod(alphas, 1)
+    acc = torch.cat((
+        torch.ones((acc.shape[0], 1), device=alphas.device), acc[:, :-1]
+    ), dim=-1)
+
+def render_rays(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=192):
+    device = ray_origins.device
+    large_num = torch.tensor([1e10], device=device)
+    t = torch.linspace(hn, hf, nb_bins, device=device).expand(ray_origins.shape[0], nb_bins)
+    # Perturb sampling along each ray.
+    mid = (t[:, :-1] + t[:, 1:]) / 2.
+    lower = torch.cat((t[:, :1], mid), -1)
+    upper = torch.cat((mid, t[:, -1:]), -1)
+    u = torch.rand(t.shape, device=device)
+    t = lower + (upper - lower) * u  # [batch_size, nb_bins]
+    delta = torch.cat((
+        t[:, 1:] - t[:, :-1],
+        large_num.expand(ray_origins.shape[0], 1)
+    ), -1)
+
+    x = ray_origins.unsqueeze(1) + t.unsqueeze(2) * ray_directions.unsqueeze(1)   # [batch_size, nb_bins, 3]
+    ray_directions = ray_directions.expand(nb_bins, ray_directions.shape[0], 3).transpose(0, 1)
+
+    colors, sigma = nerf_model(x.reshape(-1, 3), ray_directions.reshape(-1, 3))
+    colors = colors.reshape(x.shape)
+    sigma = sigma.reshape(x.shape[:-1])
+
+    alpha = 1 - torch.exp(-sigma * delta)  # [batch_size, nb_bins]
+    weights = compute_accumulated_transmittance(1 - alpha).unsqueeze(2) * alpha.unsqueeze(2)
+    c = (weights * colors).sum(dim=1)  # Pixel values
+    weight_sum = weights.sum(-1).sum(-1)  # Regularization for white background
+    return c + 1 - weight_sum.unsqueeze(-1)
